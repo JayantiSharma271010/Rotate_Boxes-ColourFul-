ffe7a49311851d87fc2b281454dc6b2162fcc6c8
Jayanti_Sharma <jayantisharma271010@gmail.com>
2023-12-20 12:05:04 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// function hello(name) {
+//     let phrase = `Hello + ${name}`
+// }
+
+// say(phrase)
+
+// function  say(phrase) {
+//     alert(`++ ${phrase} ++`)
+// }
+
+// let message = 'Hello!'
+// let phrase = message;
+
+// console.log(message)
+// console.log(phrase)
+
+// let user = {name:'John'}
+// let admin = user
+// admin.name = "Merry"
+// console.log(admin)
+// console.log(user)
+
+// let a = {}
+// let b = {}
+
+// alert(a == b)
+// alert(a === b)
+
+// shallow copy
+
+// copiedObj.b = 10
+// console.log(originalObj)
+
+// * Deep Copy
+// let originalObj = { a: 1, b: 2, c: 3 };
+// let copiedObj = Object.assign({},originalObj);
+// copiedObj.a = 10
+// console.log(copiedObj);
+// console.log(originalObj);
+
+// let user = {name: "John"}
+// Object.assign(user, {name:'Pete'})
+// alert(user.name)
